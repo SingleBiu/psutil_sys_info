89e9d4048bf6b2af309bd78088294891a32c76d3
SingleBiu <854390236@qq.com>
2023-02-21 15:31:44 +0800

    add demo

---
+++ psutil_demo.py
@@ -0,0 +1,14 @@
+import psutil
+
+print("CPU物理核心:",psutil.cpu_count(logical=False))
+print("CPU逻辑数量:",psutil.cpu_count())
+mem = psutil.virtual_memory()
+print("内存总量:",round(mem.total/1024/1024/1024,2),"GB")
+print("内存使用量:",round(mem.used/1024/1024/1024,2),"GB")
+print("内存空闲量:",round(mem.free/1024/1024/1024,2),"GB")
+print("已使用内存占比:",mem.percent,"%")
+disk_used = psutil.disk_usage('/')
+print("磁盘总量:",round(disk_used.total/1024/1024/1024,2),"GB")
+print("磁盘使用量:",round(disk_used.used/1024/1024/1024,2),"GB")
+print("磁盘空闲量:",round(disk_used.free/1024/1024/1024,2),"GB")
+print("已使用磁盘占比:",disk_used.percent,"%")
